aad4ccef526b64e3090b0aa53b5c049fc39d8c33
poojametre25 <poojametre22@gmail.com>
2025-09-05 22:29:56 +0530

    Added Student.java and Main.java(phase 1)

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student(1, "Pooja", 85);
+        Student s2 = new Student(2, "Rani", 90);
+
+        s1.displayDetails();
+        System.out.println("----");
+        s2.displayDetails();
+    }
+}
---
+++ Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private int id;
+    private String name;
+    private int marks;
+
+    public Student(int id, String name, int marks) {
+        this.id = id;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    public void displayDetails() {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Marks: " + marks);
+    }
+}
